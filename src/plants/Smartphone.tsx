d58ef994a86ab97b063f5b0c0feb3d6b7be156ba
Ulisses <iamurise@gmail.com>
2022-09-02 23:32:50 -0300

    refatorate

--- src/plants/Smartphone.tsx
+++ src/plants/Smartphone.tsx
@@ -2,7 +2,7 @@ import { ChildrenProps } from '../common/ChildrenProps';
 
 export function Smartphone({ children }:ChildrenProps) {
   return (
-    <div className="group w-[24rem] h-[38.5rem] bg-blue-500 flex items-center justify-center">
+    <div className="group w-[24rem] h-[38.5rem] flex center">
       <div className="transition-transform group-hover:rotate-0 rotate-12 scale-75 w-[23rem] h-[48rem] px-2 py-8 rounded-3xl bg-gradient-to-l from-black to-black via-gray-700">
         {children}
       </div>
