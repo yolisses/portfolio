673ae9d7dd240b5d38041670d12256636970da77
Ulisses <iamurise@gmail.com>
2022-09-11 09:15:56 -0300

    remove contact button transformation

--- src/contact/ContactButton.tsx
+++ src/contact/ContactButton.tsx
@@ -1,19 +1,11 @@
-import { useEffect, useRef, useState } from 'react';
-import { FaChevronRight, FaWhatsapp } from 'react-icons/fa';
 import './ContactButton.css';
 
 export function ContactButton() {
-  const [floating, setFloating] = useState(false);
-
   return (
     <button
-      className={`shadow p-2 contact-button flex flex-row center gap-1 transition-all ${
-        floating
-          ? 'fixed z-50 bottom-10 right-10 rounded-full text-4xl'
-          : 'rounded-lg text-lg'}`}
+      className="shadow p-2 contact-button fixed z-50 bottom-10 right-10 rounded-full"
     >
-      <FaWhatsapp color="white" className="text-white inline" />
-      {!floating && 'Contact me'}
+      Contact me
     </button>
   );
 }
