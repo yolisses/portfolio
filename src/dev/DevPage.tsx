e8bc849b356384940e57d69bfe63bcad9e6eebdb
Ulisses <iamurise@gmail.com>
2022-09-11 16:01:16 -0300

    disable iframe interaction

--- src/dev/DevPage.tsx
+++ src/dev/DevPage.tsx
@@ -1,12 +1,25 @@
-import { useEffect } from 'react';
-import { getCurrentNotes } from '../mai/getCurrentNotes';
-import { MaiExtra } from '../mai/MaiExtra';
-import { notes } from '../mai/notes';
+import { useEffect, useRef } from 'react';
 
 export function DevPage() {
+  const ref = useRef<any>();
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.contentDocument.getElementsByTagName('body')[0].setAttribute('tabindex', '-1');
+    }
+  }, [ref.current]);
   return (
-    <div className="max-w-sm">
-      <MaiExtra />
+    <div
+      className="w-[50vw] h-[50vh] relative bg-red-500"
+    >
+      <iframe
+        ref={ref}
+        src="https://suasplantas.com"
+        frameBorder="0"
+        title="plants"
+        className="w-full h-full absolute"
+        tabIndex={-1}
+      />
     </div>
   );
 }
