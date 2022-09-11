58ba3762298a1bf212d597cc90ef00d46e7bf695
Ulisses <iamurise@gmail.com>
2022-09-10 21:43:24 -0300

    refatorate intro section

--- src/home/IntroHeader.tsx
+++ src/home/IntroHeader.tsx
@@ -1,6 +1,6 @@
 export function IntroHeader() {
   return (
-    <header className="flex flex-row p-2 items-center gap-2">
+    <header className="flex flex-row p-2 items-center gap-2 w-full">
       <img
         src="/ulisses/logo.svg"
         width={32}
--- src/home/IntroSection.tsx
+++ src/home/IntroSection.tsx
@@ -5,39 +5,32 @@ export function IntroSection() {
   return (
     <section
       style={{ backgroundImage: 'url("intro/intro.jpg")' }}
-      className="text-white min-h-screen flex flex-col relative bg-cover bg-no-repeat bg-center bg-fixed"
+      className="text-white min-h-screen flex flex-col justify-between items-center bg-cover bg-no-repeat bg-center bg-fixed"
     >
       <IntroHeader />
-      <div className="flex-1 flex center">
-        <h2
-          className="text-6xl"
-          style={{ fontFamily: "'Bitter','Ubuntu','sans-serif'" }}
+      <h2
+        className="text-6xl"
+        style={{ fontFamily: "'Bitter','Ubuntu','sans-serif'" }}
+      >
+        Recruit who goes the extra mile
+      </h2>
+      <div>
+        <a
+          className="hover:underline"
+          href="https://github.com/yolisses"
         >
-          Recruit who goes the extra mile
-        </h2>
-      </div>
-      <div className="flex flex-row justify-around items-end">
-        <div className="p-4 flex flex-col gap-1">
-          <div>
-            <a
-              className="hover:underline"
-              href="https://github.com/yolisses"
-            >
-              yolisses
-            </a>
-          </div>
-          <GitHubCalendar
-            username="yolisses"
-            theme={{
-              level0: '#161b2288',
-              level1: '#0e4429',
-              level2: '#006d32',
-              level3: '#26a641',
-              level4: '#39d353',
-            }}
-          />
-        </div>
-        {/* <img src="/ulisses/ulisses.png" alt="" className="saturate-150" /> */}
+          yolisses
+        </a>
+        <GitHubCalendar
+          username="yolisses"
+          theme={{
+            level0: '#161b2288',
+            level1: '#0e4429',
+            level2: '#006d32',
+            level3: '#26a641',
+            level4: '#39d353',
+          }}
+        />
       </div>
     </section>
   );
