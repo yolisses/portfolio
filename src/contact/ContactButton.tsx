ed6ad262a21a4531847b3e01d15150b244b8a64a
Ulisses <iamurise@gmail.com>
2022-09-11 16:00:57 -0300

    make contact button blue

--- src/contact/ContactButton.tsx
+++ src/contact/ContactButton.tsx
@@ -3,7 +3,7 @@ import './ContactButton.css';
 export function ContactButton() {
   return (
     <button
-      className="shadow p-2 contact-button fixed z-10 bottom-10 right-10 rounded-full"
+      className="shadow p-2 contact-button fixed z-10 text-white bottom-10 right-10 rounded-full"
     >
       Contact me
     </button>
