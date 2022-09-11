f03fb9143530879855c98224593cc2344f3e6407
Ulisses <iamurise@gmail.com>
2022-09-10 22:37:44 -0300

    add whatsapp logo to contact button

--- src/contact/ContactButton.tsx
+++ src/contact/ContactButton.tsx
@@ -1,11 +1,11 @@
-import { FaChevronRight } from 'react-icons/fa';
+import { FaChevronRight, FaWhatsapp } from 'react-icons/fa';
 import './ContactButton.css';
 
 export function ContactButton() {
   return (
-    <button className="rounded-lg shadow p-2 pl-4 text-lg contact-button flex flex-row center">
-      Call me
-      <FaChevronRight className="inline" />
+    <button className="rounded-lg shadow p-2 text-lg contact-button flex flex-row center gap-1">
+      <FaWhatsapp color="white" className="text-white inline" />
+      Contact me
     </button>
   );
 }
