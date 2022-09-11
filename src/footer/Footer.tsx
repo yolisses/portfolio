037858141f7625cbea4481f742e300a0d9e5634d
Ulisses <iamurise@gmail.com>
2022-09-10 21:25:47 -0300

    add contact info to footer

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import {
-  FaEnvelope, FaGithub, FaHeart, FaRegEnvelope,
+  FaEnvelope, FaGithub, FaHeart, FaLinkedin, FaRegEnvelope, FaWhatsapp,
 } from 'react-icons/fa';
 
 export function Footer() {
@@ -8,40 +8,74 @@ export function Footer() {
       style={{
         backgroundImage: 'url("footer/footer.jpg")',
       }}
-      className="text-white gap-2 justify-around bg-gray-900 bg-cover bg-center bg-fixed p-4 pt-20 grid"
+      className="text-white gap-2 text-sm justify-around bg-gray-900 bg-cover bg-center bg-fixed p-4 pt-10 grid grid-flow-col"
     >
-      <a
-        target="_blank"
-        rel="noreferrer"
-        className="hover:underline"
-        href="https://github.com/yolisses/portfolio"
-      >
-        <FaGithub className="inline" />
-        {' '}
-        Want this page code?
-      </a>
-      <div>
-        Crafted with
-        {' '}
-        <img
-          alt="love"
-          width={18}
-          className="inline"
-          src="footer/heart.png"
-          style={{ imageRendering: 'pixelated' }}
-        />
+      <div className="flex flex-col gap-2">
+        <div>
+          Crafted with
+          {' '}
+          <img
+            alt="love"
+            width={18}
+            className="inline"
+            src="footer/heart.png"
+            style={{ imageRendering: 'pixelated' }}
+          />
+        </div>
+        <div>
+          © Ulisses Pereira 2022
+        </div>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="hover:underline"
+          href="https://github.com/yolisses/portfolio"
+        >
+          Want this page code?
+        </a>
       </div>
-      <div>
-        © Ulisses Pereira 2022
+      <div className="flex flex-col gap-2">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="hover:underline"
+          href="https://wa.me/558393450389?text=Hi!%20I%20saw%20your%20portfolio%20and%20want%20to%20have%20a%20talk"
+        >
+          <FaWhatsapp className="inline" />
+          {' '}
+          +55 83 9345-0389
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="hover:underline"
+          href="mailto:iamurise@gmail.com"
+        >
+          <FaRegEnvelope className="inline" />
+          {' '}
+          iamurise@gmail.com
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="hover:underline"
+          href="https://github.com/yolisses"
+        >
+          <FaGithub className="inline" />
+          {' '}
+          yolisses
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="hover:underline"
+          href="https://www.linkedin.com/in/yolisses"
+        >
+          <FaLinkedin className="inline" />
+          {' '}
+          Ulisses Pereira
+        </a>
       </div>
-      <a
-        className="hover:underline"
-        href="mailto:iamurise@gmail.com"
-      >
-        <FaRegEnvelope className="inline" />
-        {' '}
-        iamurise@gmail.com
-      </a>
     </footer>
   );
 }
