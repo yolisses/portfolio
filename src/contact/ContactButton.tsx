734ad275adb04f877523b7543f152a6654b35b9a
Ulisses <iamurise@gmail.com>
2022-09-10 23:16:23 -0300

    create contact button floating form

--- src/contact/ContactButton.tsx
+++ src/contact/ContactButton.tsx
@@ -1,11 +1,19 @@
+import { useEffect, useRef, useState } from 'react';
 import { FaChevronRight, FaWhatsapp } from 'react-icons/fa';
 import './ContactButton.css';
 
 export function ContactButton() {
+  const [floating, setFloating] = useState(false);
+
   return (
-    <button className="rounded-lg shadow p-2 text-lg contact-button flex flex-row center gap-1">
+    <button
+      className={`shadow p-2 contact-button flex flex-row center gap-1 transition-all ${
+        floating
+          ? 'fixed z-50 bottom-10 right-10 rounded-full text-4xl'
+          : 'rounded-lg text-lg'}`}
+    >
       <FaWhatsapp color="white" className="text-white inline" />
-      Contact me
+      {!floating && 'Contact me'}
     </button>
   );
 }
