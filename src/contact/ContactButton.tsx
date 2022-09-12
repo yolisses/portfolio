da938237706e23fab0dd4055884fe94c0e4c927b
Ulisses <iamurise@gmail.com>
2022-09-12 10:48:27 -0300

    style contact button

--- src/contact/ContactButton.tsx
+++ src/contact/ContactButton.tsx
@@ -3,7 +3,7 @@ import './ContactButton.css';
 export function ContactButton() {
   return (
     <button
-      className="shadow p-2 contact-button fixed z-10 text-white bottom-10 right-10 rounded-full"
+      className="shadow p-2 contact-button fixed z-10 text-white bottom-5 right-5 md:bottom-10 md:right-10 rounded-full"
     >
       Contact me
     </button>
